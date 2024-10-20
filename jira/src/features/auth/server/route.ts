b7e5588c64e55b726003f9c310021609e33b1a89
JangSeung <0sunghee122@gmail.com>
2024-10-20 19:34:07 +0900

    .

--- jira/src/features/auth/server/route.ts
+++ jira/src/features/auth/server/route.ts
@@ -1,17 +1,16 @@
 import { z } from "zod";
 import { Hono } from "hono";
 import { zValidator } from "@hono/zod-validator";
+import { loginSchema } from "../schema";
 
 const app = new Hono()
     .post(
         "/login",
-        zValidator("json", z.object({
-            email: z.string().email(),
-            password: z.string(),
-        })),
+        zValidator("json", loginSchema),
          (c) => {
         return c.json({ success: "ok"});
         }
     );
 
 export default app;
+
