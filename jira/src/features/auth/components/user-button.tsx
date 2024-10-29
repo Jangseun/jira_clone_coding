d2a9f043262ae872871b3af0264d08b58d1ef35e
JangSeung <0sunghee122@gmail.com>
2024-10-29 19:29:14 +0900

    .

---
+++ jira/src/features/auth/components/user-button.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+import { Avatar, AvatatFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
