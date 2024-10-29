dbd535f498606efc8575c93439cf7bd5abf146e5
JangSeung <0sunghee122@gmail.com>
2024-10-29 20:49:14 +0900

    .

--- jira/src/app/(auth)/sign-in/page.tsx
+++ jira/src/app/(auth)/sign-in/page.tsx
@@ -1,8 +1,14 @@
-"use client";
+import { redirect } from "next/navigation";
 
+import { getCurrent } from "@/features/auth/actions";
 import { SignInCard } from "@/features/auth/components/sign-in-card";
 
-const SingnInPage = () =>{
+
+const SingnInPage = async () => {
+    const user = await getCurrent();
+
+    if (user) redirect("/");
+
     return <SignInCard />
 };
 
