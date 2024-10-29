a90d6a1c8d5552c8c0f40882f4c8b45613d6f521
JangSeung <0sunghee122@gmail.com>
2024-10-29 20:56:08 +0900

    .

--- jira/src/app/(auth)/sign-up/page.tsx
+++ jira/src/app/(auth)/sign-up/page.tsx
@@ -7,6 +7,8 @@ import { SignUpCard } from "@/features/auth/components/sign-up-card";
 const SingnUpPage = async () =>{
     const user = await getCurrent();
 
+    console.log({user})
+
     if (user) redirect("/");
 
     return <SignUpCard />
