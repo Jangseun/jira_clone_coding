49ca3534c9255a4feacf2a5fe08e9a479406a473
JangSeung <0sunghee122@gmail.com>
2024-10-30 20:30:07 +0900

    .

--- jira/src/app/(dashboard)/layout.tsx
+++ jira/src/app/(dashboard)/layout.tsx
@@ -7,8 +7,16 @@ const DashboardLayout = ({ children }: DashboardLayoutProps) => {
     return (
         <div className="min-h-screen">
             <div className="flex w-full h-full">
+                <div className="flixed left-0 hidden lg:block lg:w-[264px]">
+
+                </div>
                 <div className="lg:pl-[264px]">
-                    {children}
+                    <div className="mx-auto max-w-screen-2xl h-full">
+                        {/* TODO: Navbar */}
+                        <main className="h-full py-8 px-6 flex flex-col">
+                            {children} 
+                        </main>
+                    </div>
                 </div>
             </div>
         </div>
