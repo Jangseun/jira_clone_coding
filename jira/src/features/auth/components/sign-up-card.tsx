8341e3eb273d875768cf271e502a06e85e028375
JangSeung <0sunghee122@gmail.com>
2024-10-30 18:53:45 +0900

    .

--- jira/src/features/auth/components/sign-up-card.tsx
+++ jira/src/features/auth/components/sign-up-card.tsx
@@ -30,7 +30,7 @@ import { registerSchema } from "../schema";
 import { useRegister } from "../api/use-register";
 
 export const SignUpCard  = () => {
-    const  { mutate } = useRegister();
+    const  { mutate, isPending } = useRegister();
     const form = useForm<z.infer<typeof registerSchema>>({
         resolver: zodResolver(registerSchema),
         defaultValues: {
@@ -115,7 +115,7 @@ export const SignUpCard  = () => {
                                 </FormItem>
                             )}
                         />
-                        <Button disabled={false} size="lg" className="w-full">
+                        <Button disabled={isPending} size="lg" className="w-full">
                             Register
                         </Button>
                     </form>
@@ -126,19 +126,21 @@ export const SignUpCard  = () => {
             </div>
             <CardContent className="p-7 flex flex-col gap-y-4">
                 <Button 
+                disabled = {isPending}
                 variant="secondary"
                 size="lg"
                 className="w-full"
                 >
-                    <div className = "mr-2 size-5">
-                        <FcGoogle size="full"/>
-                    </div>
-                        Login with Google
-                    </Button>
-                        <Button 
-                        variant="secondary"
-                        size="lg"
-                        className="w-full"
+                <div className = "mr-2 size-5">
+                    <FcGoogle size="full"/>
+                </div>
+                Login with Google
+                </Button>
+                <Button 
+                disabled={isPending}
+                variant="secondary"
+                size="lg"
+                className="w-full"
                     >
                     <div className = "mr-2 size-5">
                         <FaGithub size="full"/>
