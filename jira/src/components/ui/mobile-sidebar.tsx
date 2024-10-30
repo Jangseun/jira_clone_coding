99f4a43f535533df9c961165a70cab8e69c4f3f6
JangSeung <0sunghee122@gmail.com>
2024-10-30 22:21:53 +0900

    .

---
+++ jira/src/components/ui/mobile-sidebar.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { useState } from "react";
+import { MenuIcon } from "lucide-react";
+
+import { Sidebar } from "./sidebar";
+import { Button } from "../ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "../ui/sheet";
+import { usePathname } from "next/navigation";
+
+export const MobileSidebar = () => {
+    const [isOpen, setOpoen] = useState(false);
+    const pathname = usePathname();
+    return(
+        <Sheet modal={false}>
+            <SheetTrigger asChild>
+                <Button variant="secondary" className="lg:hidden">
+                    <MenuIcon className="size-4 text-neutral-500" />
+                </Button>
+            </SheetTrigger>
+            <SheetContent side="left" className="p-0">
+                <Sidebar />
+            </SheetContent>
+        </Sheet>
+    );
+};
