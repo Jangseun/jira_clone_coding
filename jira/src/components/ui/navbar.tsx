12fa4362219e61d88d073a695c824d2d21472ea5
JangSeung <0sunghee122@gmail.com>
2024-10-30 22:50:12 +0900

    .

--- jira/src/components/ui/navbar.tsx
+++ jira/src/components/ui/navbar.tsx
@@ -5,7 +5,7 @@ import { MobileSidebar } from "./mobile-sidebar";
 export const Navbar = () => {
     return(
         <nav className="pt-4 px-6 flex items-center justify-between">
-            <div className="flex-col hiddem lg:flex">
+            <div className="flex-col hidden lg:flex">
                 <h1 className="text-2xl font-semibold">Home</h1>
                 <p className="text-muted-foreground">Monitor all of yourt projects and tasks here</p>
             </div>
